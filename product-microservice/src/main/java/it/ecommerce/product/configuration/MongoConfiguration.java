f91a5b31b1fff739e35ec164238d0406e4a66f57
Davide Ventriglia <davide.ventriglia.swd@gmail.com>
2022-12-16 16:18:16 +0100

    created mvn-repo branch

--- product-microservice/src/main/java/it/ecommerce/product/configuration/MongoConfiguration.java
+++
@@ -1,8 +0,0 @@
-package it.ecommerce.product.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@Configuration
-@EnableMongoRepositories(basePackages = "it.ecommerce.product.repository")
-public class MongoConfiguration {}
